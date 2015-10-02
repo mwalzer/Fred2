c8712a3ab7a3b613dc416dd89125a3ce46cc2991
mwalzer <walzer@informatik.uni-tuebingen.de>
2015-10-02 14:05:10 +0200

    added first version of generate_peptides_from_proteins(only_variants)

--- Fred2/Core/Generator.py
+++ Fred2/Core/Generator.py
@@ -505,7 +505,7 @@ def generate_proteins_from_transcripts(transcripts, table='Standard', stop_symbo
 #        P R O T E I N    = = >    P E P T I D E
 ################################################################################
 
-def generate_peptides_from_protein(proteins, window_size, peptides=None):
+def generate_peptides_from_protein(proteins, window_size, peptides=None, only_variants=False):
     """
     Creates all peptides for a given window size, from a given protein. The
     result is a generator.
@@ -522,12 +522,13 @@ def generate_peptides_from_protein(proteins, window_size, peptides=None):
         res = []
 
         seq = str(protein)
-        for i in xrange(len(protein)+1-window_size):
-            # generate peptide fragment
-            end = i+window_size
-            pep_seq = seq[i:end]
-
-            res.append((pep_seq, i))
+        for i in xrange(len(protein) + 1 - window_size):
+            if (only_variants and any([i <= vp <= i + window_size - 1 for vp in protein.vars.keys()])) \
+                    or not only_variants: #works quirky with frameshifts ... (if mut does not change first aa, will create one peptide which is same as wildtype)
+                # generate peptide fragment
+                end = i + window_size
+                pep_seq = seq[i:end]
+                res.append((pep_seq, i))
         return res
 
     if isinstance(peptides, Peptide):
@@ -536,7 +537,7 @@ def generate_peptides_from_protein(proteins, window_size, peptides=None):
     if peptides and any(not isinstance(p, Peptide) for p in peptides):
         raise ValueError("Specified list of Peptides contain non peptide objects")
 
-    final_peptides = {} if peptides is None else {str(p):p for p in peptides}
+    final_peptides = {} if peptides is None else {str(p): p for p in peptides}
 
     if isinstance(proteins, Protein):
         proteins = [proteins]
